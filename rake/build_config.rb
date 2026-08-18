682ae6c92dda9242bb4f8eb5ed1729c9d6f000e2
Chadowo <83732118+Chadowo@users.noreply.github.com>
2026-08-18 18:33:42 -0300

    Add support for defines in `BuildConfig`
    Also changed a little bit the log output
    
    Signed-off-by: Chadowo <83732118+Chadowo@users.noreply.github.com>

--- rake/build_config.rb
+++ rake/build_config.rb
@@ -1,26 +1,46 @@
 require 'fileutils'
 
 class BuildConfig
-  attr_accessor :name, :flags, :files, :include_dirs, :link_dirs, :out_filename
+  attr_accessor :name, :flags, :files, :include_dirs, :link_dirs, :out_filename, :silent
 
   def initialize(name)
     @name = name
 
     @flags = []
     @files = []
+    @defines = []
 
     @include_dirs = []
 
     @out_filename = ''
+    @silent = false
 
     yield self # I'd give my body for you, my liege
   end
 
-  def build
-    command = "fpc #{@files.join(' ')} #{@flags.join(' ')} #{@include_dirs.each { |d| d.prepend('-Fu')}.join(' ')} -FUbuild/units -FEbuild/ -o#{@out_filename}"
+  def enable_debug
+    @flags << '-g' # GDB debug info
+    @flags << '-gl' # Lineinfo unit for line information
 
-    write_summary
-    puts "Final command: #{command}"
+    # Checks
+    @flags << '-Ci' # I/O
+    @flags << '-Co' # Overflow of integer operations
+    @flags << '-CO'
+    @flags << '-Cr' # Range checking
+    @flags << '-CR' # Verify object method call validity
+
+    #@flags << '-gv' # Valgrind debug info
+
+    @defines << '-dDEBUG'
+  end
+
+  def build
+    command = "fpc #{@files.join(' ')} #{@flags.join(' ')} #{@defines.join(' ')} #{@include_dirs.each { |d| d.prepend('-Fu') }.join(' ')} -FUbuild/units -FEbuild/ -o#{@out_filename}"
+
+    unless @silent
+      write_summary
+      puts "Final command: #{command}"
+    end
 
     create_build_directory
     system(command)
@@ -49,11 +69,10 @@ class BuildConfig
   def write_summary
     puts <<~SUMMARY
       Build config: "#{@name}"
-      FPC info: #{`fpc -v`.lines.first.chomp}
-      -------------------------------------------------------------------------
       * Flags: #{@flags}
       * Files: #{@files}
       * Include dirs: #{@include_dirs}
+      -------------------------------------------------------------------------
     SUMMARY
   end
 end
